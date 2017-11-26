f447ffbab68528bd51e0272d1e3433336753f086
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2017-11-25 20:33:36 -0500

    Cleaned rep
    Cleaned the rep and create the geolocation.exe

--- createjsonfile.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def createJsonFile(array):
-    f = open("data.json","w+")
-    with f as output:
-        jsonData = json.dumps(array)
-        with f as a:
-            json.dump(jsonData, a)
---
+++ geolocation.py/geolocation.py
@@ -0,0 +1,27 @@
+import urllib.request
+import json
+
+def createJsonFile(info_array):
+    for thing in info_array:
+        thing = str(thing)
+    f = open("data.json","w+")
+    with f as output:
+        jsonData = json.dumps(info_array)
+        with f as a:
+            json.dump(jsonData, a)
+
+# Automatically geolocate the connecting IP
+f = urllib.request.urlopen('http://freegeoip.net/json/')
+json_string = f.read()
+f.close()
+
+
+location = json.loads(json_string)
+location_city = location['city']
+location_state = location['region_name']
+location_country = location['country_name']
+location_latitude = location['latitude']
+location_longitude = location['longitude']
+
+info_array = [location_country, location_longitude, location_latitude]
+createJsonFile(info_array)
