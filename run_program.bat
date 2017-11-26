ec09632495b70a99b226e6b648227b300d29ef21
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2017-11-25 23:38:07 -0500

    dw about it

---
+++ run_program.bat
@@ -0,0 +1,9 @@
+run_program.bat
+@echo off
+cd "C:\Program Files\Ethereum-Wallet"
+start "Ethereum Wallet.exe"
+
+cd "C:\Users\yunke\Documents\PycharmProjects\BlocHacks\geolocation"
+start geolocation.exe
+
+exit
